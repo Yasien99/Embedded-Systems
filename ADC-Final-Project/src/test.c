6723c4ccd09f969e8243cc7c66d72e53c85a27f6
Yasien Essam <yasien.essam99@gmail.com>
2022-06-07 01:36:34 +0200

    add adc final

---
+++ ADC-Final-Project/src/test.c
@@ -0,0 +1,21 @@
+//#include<stdint.h>
+//#include "stm32f4xx.h"
+//#include "gpio.h"
+//
+//
+//int main(void)
+//{
+//	GPIO_Init(GPIOA,4,1,0);
+//	// super loop
+//
+//	while(1)
+//	{
+//
+//		GPIO_Write(GPIOA,4,0);
+//		for(int i =0 ; i<500000;i++){}
+//		GPIO_Write(GPIOA,4,1);
+//		for(int i =0 ; i<500000;i++){}
+//
+//	}
+//
+//}
